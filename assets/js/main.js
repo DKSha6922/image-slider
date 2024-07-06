197962fbfa1fae145fa6a25b614b4fbbc6e3b67e
Mohammedshafi <shafiking6922@gmail.com>
2024-07-06 12:10:22 +0530

    change: get element from html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,25 @@
 // Write your javascript code here
+let count = 0;
+
+const leftBtn = document.getElementById("left");
+const rightBtn = document.getElementById("right");
+const container = document.getElementById("container");
+const items = Array.from(container.children);
+
+ 
+
+
+
+//i click next btn increase in array value count if count > 6 starting increasing count from 1
+rightBtn.addEventListener("click",()=>{
+  
+})
+
+//now i click left button decrease count countinu value
+leftBtn.addEventListener("click",()=>{
+count--;
+if(count < 0){
+    count = values.length-1;
+}
+display.innerText = values[count];
+})
